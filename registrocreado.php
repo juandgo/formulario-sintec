ffad919523184371a7efe7e3306079bf71c91038
camilo20000 <juandavidgo1997@gmail.com>
2024-03-15 09:36:15 -0500

    registrocreado add

---
+++ registrocreado.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Formulario de Inscripcón</title>
+</head>
+
+<body>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center py-3">
+                <img src="img/fesco.png" class="img-responsive text-center" alt="">
+            </div>
+            <div class="col-12 text-center">
+                <h2 class="fw-bold text-center py-5">Inscripción exitosa</h2>
+                <hr>
+                <!-- <form action=""> -->
+                    <h4 class="text-center">¿Quieres inscribir a otra persona deseas volver al inicio?</h4>
+                    <div class="col-12 text-center py-5">
+                        <a class="btn btn-primary fw-bold" href="formulario.html">Inscribir otra persona</a>
+
+                        <a class="btn btn-success fw-bold" href="index.html">Ir a Inicio</a>
+                    </div><br>
+                <!-- </form> -->
+            </div>
+        </div>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</html>
